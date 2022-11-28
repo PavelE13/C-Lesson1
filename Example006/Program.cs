bd23cf7814dc8e6c8c906b71e9673f5d7e308fc0
Benny Elashkin <pavel.elashkin@gmail.com>
2022-11-28 22:27:56 +0300

    Add example006

---
+++ Example006/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username=Console.ReadLine();
+if (username.ToLower()=="маша") {
+    Console.Write("УРРА! Это Маша! ");
+}
+else {
+    Console.Write("Привет " + username);
+}
